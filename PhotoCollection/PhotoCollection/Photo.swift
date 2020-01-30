0ab45ca15bd62d5519a70e5641423c6728e73dfc
Stephanie Ballard <user.name>
2020-01-30 11:54:05 -0800

    set up model photo struct

---
+++ PhotoCollection/PhotoCollection/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  PhotoCollection
+//
+//  Created by Stephanie Ballard on 1/30/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import Foundation
+
+struct Photo: Equatable {
+    var imageData: Data
+    var title: String
+}
